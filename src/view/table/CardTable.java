65e02beb0b6947df284e38bc63d3226c7273cd3f
jeewantha1234 <jeewanthafernando925@gmail.com>
2021-12-28 13:23:06 +0530

    table

--- src/view/table/CardTable.java
+++ src/view/table/CardTable.java
@@ -1,4 +1,71 @@
 package view.table;
 
 public class CardTable {
+    private String code;
+    private String item;
+    private double unitPrice;
+    private int qty;
+    private double total;
+
+    public CardTable() {
+    }
+
+    public CardTable(String code, String item, double unitPrice, int qty, double total) {
+        this.code = code;
+        this.item = item;
+        this.unitPrice = unitPrice;
+        this.qty = qty;
+        this.total = total;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "CardTable{" +
+                "code='" + code + '\'' +
+                ", item='" + item + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qty=" + qty +
+                ", total=" + total +
+                '}';
+    }
 }
--- src/view/table/ItemTable.java
+++ src/view/table/ItemTable.java
@@ -1,4 +1,60 @@
 package view.table;
 
 public class ItemTable {
+    private String code;
+    private String item;
+    private double price;
+    private int qty;
+
+    public ItemTable() {
+    }
+
+    public ItemTable(String code, String item, double price, int qty) {
+        this.code = code;
+        this.item = item;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemTable{" +
+                "code='" + code + '\'' +
+                ", item='" + item + '\'' +
+                ", price=" + price +
+                ", qty=" + qty +
+                '}';
+    }
 }
